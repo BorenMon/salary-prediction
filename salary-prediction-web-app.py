1ea4a32822aa9ca885bf7965c590d6ff5b15b165
Boren Mon <borenmon5@gmail.com>
2022-12-16 01:13:25 +0700

    Update salary-prediction-web-app.py

--- salary-prediction-web-app.py
+++ salary-prediction-web-app.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 # Loading the saved model
-loaded_model = pickle.load(open('C:/Users/Boren/Desktop/phyton/trained-model.sav', 'rb'))
+loaded_model = pickle.load(open('trained-model.sav', 'rb'))
 
 # Creating a function for prediction
 def salary_prediction(input_data):
